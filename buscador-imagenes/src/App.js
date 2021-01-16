0e990f1fe4e50127b05ff1ede3b68fdb0ff3f0ca
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2021-01-16 13:26:09 +0100

    Anhadimos la API y creamos el donde se almacenen las img encontradas

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -3,8 +3,29 @@ import Buscador from "./componentes/Buscador";
 
 // Componente principal
 class App extends Component {
+  // Tambien se puede usar el constructor en vez del state
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  consultarAPI = () => {
+    // API PIXABAY, PASAMOS EL TERMINO DEL BUSCADOR
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${termino}&per_page=30`;
+    // console.log(url);
+    // Funcion fetch para recoger todos lo json en un array asociativo 
+    fetch(url)
+      .then(respuesta => respuesta.json() )
+      .then(resultado => this.setState({ imagenes : resultado.hits }) )
+  }
+
   datosBusqueda = (termino) => {
-    console.log(termino);
+    // Modificamos la propiedad termino del objeto state y ejecutamos el metodo consultarAPI
+    this.setState(
+      {termino}, () => {
+        this.consultarAPI();
+      });
   };
 
   render() {
@@ -15,6 +36,7 @@ class App extends Component {
           <p className="lead text-center">Buscador de Imágenes</p>
           <Buscador datosBusqueda={this.datosBusqueda} />
         </div>
+        {this.state.termmino}
       </div>
     );
   }
